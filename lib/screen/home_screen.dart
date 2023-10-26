4302a2cf626d26e72a7ad8ed74c14969c6fb41a1
joon shim <joonshim@joonui-MacBookPro-2.local>
2023-10-26 18:49:31 +0900

    신규 프로젝트 생성

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:toon/model/webtoon_model.dart';
+import 'package:toon/service/api_service.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoon = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    webtoon = await ApiService.getDoaysToos();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
